a0aa2eb806a2c9f0c058fb7b9c08ff2160803b70
parasharparth <parthparashar6@gmail.com>
2022-03-19 05:19:25 -0700

    Bubble sorting

--- Algorithms/src/practice/BubbleSorting2.java
+++ Algorithms/src/practice/BubbleSorting2.java
@@ -9,6 +9,18 @@ public class BubbleSorting2 {
 	
 	public static int[] bubbleSort(int[] arr)
 	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j] > arr[j+1])
+				{
+				    int temp = arr[j];
+				    arr[j] = arr[j+1];
+				    arr[j+1] = temp;
+				}
+			}
+		}
 		return arr;
 	}
 }
