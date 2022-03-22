c64cffdc2981df5341549d606231172a56c20363
parasharparth <parthparashar6@gmail.com>
2022-03-21 18:43:56 -0700

    Insertion sorting

--- Algorithms/src/practice/InsertionSorting.java
+++ Algorithms/src/practice/InsertionSorting.java
@@ -4,13 +4,26 @@ public class InsertionSorting {
 
 	public static void main(String args[])
 	{
-		int[] arr = InputArray.input();
-		
-		insertionSort(arr);
+				DisplayArray.result(insertionSort(InputArray.input()));
 	}
 	
-	public static void insertionSort(int[] arr)
+	public static int[] insertionSort(int[] arr)
 	{
-		
+		//Now, here we have to take the array and make new positions
+		for(int i=1;i<arr.length-1;++i)
+		{
+			int j=i-1;
+			int key = arr[i];
+			
+			//Now, we have to check the elements which are behind the current element, represented by i
+			// and create the space required for insertion 
+			while(j>=0 && arr[j] > key)
+			{
+				arr[j+1] = arr[j];
+				j=j-1;
+			}
+			arr[j+1] = key;
+		}
+		return arr;
 	}
 }
---
+++ Algorithms/src/practice/InsertionSorting2.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class InsertionSorting2 {
+
+	public static void main(String args[])
+	{
+		DisplayArray.result(insertionSort(InputArray.input()));
+	}
+	
+	public static int[] insertionSort(int[] arr)
+	{
+		for(int i=1; i<arr.length;++i)
+		{
+			int j=i-1;
+			int key = arr[i];
+			
+			while(j>=0 && arr[j] > key)
+			{
+				arr[j+1] = arr[j];
+				j=j-1;
+			}
+			arr[j+1] = key;
+		}
+		return arr;
+	}
+}
