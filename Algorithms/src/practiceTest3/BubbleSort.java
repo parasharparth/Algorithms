abacffb390e8bb19658a9b1ea83f32414b0863d2
parasharparth <parthparashar6@gmail.com>
2022-04-04 15:55:49 -0700

    Selection and bubble sort

--- Algorithms/src/practiceTest3/BubbleSort.java
+++ Algorithms/src/practiceTest3/BubbleSort.java
@@ -1,5 +1,41 @@
 package practiceTest3;
 
+import util.DisplayArray;
+import util.RandomisedArray;
+
 public class BubbleSort {
 
+	public static void main(String args[])
+	{
+		//The first step is to get the randomised array.
+		int[] arr = RandomisedArray.getRandomizedArray();
+		
+		//Display the Array
+		DisplayArray.result(arr);
+		
+		//Call the bubble sort
+		int[] result = bubbleSorting(arr);
+		
+		//Display the sorted array
+		DisplayArray.result(result);
+	}
+	
+	public static int[] bubbleSorting(int[] arr)
+	{
+		//Here elements are compared in pairs and then the iterator moves forward until the end condition is reached
+		for(int i=0; i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j] > arr[j+1])
+				{
+					//swap the numbers
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		return arr;
+	}
 }
--- Algorithms/src/practiceTest3/SelectionSort.java
+++ Algorithms/src/practiceTest3/SelectionSort.java
@@ -1,5 +1,37 @@
 package practiceTest3;
 
+import util.DisplayArray;
+import util.RandomisedArray;
+
 public class SelectionSort {
 
+	public static void main(String args[])
+	{
+		//The first step is to get the randomized array
+		int[] arr = RandomisedArray.getRandomizedArray();
+		
+		//The second step is to call the selectionSorting method
+		int[] result = selectionSorting(arr);
+		
+		//The third step is to display the sorted array
+		DisplayArray.result(result);
+	}
+	
+	public static int[] selectionSorting(int[] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[j] < arr[i])
+				{
+					//swap the elements
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		return arr;
+	}
 }
