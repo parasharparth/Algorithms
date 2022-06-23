ed3d9b836d24dff7edfedfd66371b888d08a4fee
parasharparth <parthparashar6@gmail.com>
2022-06-22 23:54:49 -0700

    June-22-Commit-2-Sorting

---
+++ Algorithms/src/June22/OptimizedBubbleSort.java
@@ -0,0 +1,19 @@
+package June22;
+
+public class OptimizedBubbleSort {
+    
+    public static void main(String args[])
+    {
+        int[] arr = {1,9,8,3,4,6,2,7,5,0};
+
+        //int[] result = optimizedBubbleSorting(arr);
+
+        //System.out.println("The sorted array is:");
+        //for(int i=0; i<result.length;i++)
+        //{
+        //    System.out.println(result[i]);
+       // }
+    }
+
+
+}
