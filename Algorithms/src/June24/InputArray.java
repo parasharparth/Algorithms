602eb169b1dcdb7b7d1859c85af1579ffb144cac
parasharparth <parthparashar6@gmail.com>
2022-06-24 14:09:37 -0700

    June-24-Searching

---
+++ Algorithms/src/June24/InputArray.java
@@ -0,0 +1,9 @@
+package June24;
+
+public class InputArray {
+
+    public static int[] input()
+    {
+        return new int[]{1, 9, 8, 3, 4, 6, 2, 7, 5, 0};
+    }
+}
---
+++ Algorithms/src/june23/QuickSort.java
@@ -0,0 +1,9 @@
+package june23;
+
+public class QuickSort {
+
+    public static void main(String args[])
+    {
+
+    }
+}
