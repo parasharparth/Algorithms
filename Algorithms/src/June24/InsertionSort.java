6fbf71fdf3ce96a42efc9169f24b85c6dd9c39a9
parasharparth <parthparashar6@gmail.com>
2022-06-24 17:39:31 -0700

    June-24-Sorting

---
+++ Algorithms/src/June24/InsertionSort.java
@@ -0,0 +1,30 @@
+package June24;
+
+public class InsertionSort {
+
+    public static void main(String args[])
+    {
+        System.out.println("the unsorted array is:");
+        int[] arr = InputArray.input();
+        PrintArray.printArray(arr);
+        System.out.println();
+        System.out.println("the sorted array is:");
+        int[] result = insertionSort(arr);
+        PrintArray.printArray(result);
+    }
+    public static int[] insertionSort(int[] arr)
+    {
+        for(int i=1; i<arr.length;i++)
+        {
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && key > arr[j]) //This key </> determines the ascending/descending order
+            {
+                arr[j+1] = arr[j];
+                --j;
+            }
+            arr[j+1] =  key;
+        }
+        return arr;
+    }
+}
