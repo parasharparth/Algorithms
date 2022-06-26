518c0537a165bfcf30e1d47e1ab8872113302ab7
parasharparth <parthparashar6@gmail.com>
2022-06-26 03:51:32 -0700

    June-25-Sorting-3

---
+++ Algorithms/src/June25/sorting/QuickSort.java
@@ -0,0 +1,51 @@
+package June25.sorting;
+
+import June25.utils.InputArray;
+import June25.utils.Output;
+import June25.utils.PrintArray;
+
+public class QuickSort {
+
+    public static void main(String args[])
+    {
+        Output.output("The unsorted array is: ");
+        PrintArray.printArray(InputArray.input());
+        Output.output("The sorted array is: ");
+        int[] arr = InputArray.input();
+        PrintArray.printArray(quickSort(arr,0,arr.length-1));
+    }
+
+    public static int[] quickSort(int[] arr, int low, int high)
+    {
+        if(low < high)
+        {
+            int pivot = partition(arr,low,high);
+            quickSort(arr,low,pivot-1);
+            quickSort(arr,pivot+1,high);
+        }
+        return arr;
+    }
+
+    public static int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int initial = low-1;
+
+        for(int i=low; i<high;i++)
+        {
+            if(arr[i] <= pivot)
+            {
+                initial++;
+                int temp = arr[initial];
+                arr[initial] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        //swap the pivot element with the current value of initial which is greater than the pivot.
+        int temp = arr[initial+1];
+        arr[initial+1] = arr[high];
+        arr[high] = temp;
+
+        return initial+1;
+    }
+}
