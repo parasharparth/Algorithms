01eaa0f917de25cb4afae8f0b71474f8dfd59cf7
parasharparth <parthparashar6@gmail.com>
2022-07-01 04:02:37 -0700

    July-01-commit-1

---
+++ Algorithms/src/July01/searching/RecursiveBinarySearch.java
@@ -0,0 +1,43 @@
+package July01.searching;
+
+import java.util.Arrays;
+
+public class RecursiveBinarySearch {
+
+    public static void main(String args[])
+    {
+        int[] arr = {0,1,9,2,8,3,7,4,6,5};
+
+        int result = binarySearch(arr,5,0,arr.length);
+
+        if(result == -1)
+        {
+            System.out.println("Element not found");
+        }
+        else {
+            System.out.println("Element found at position: "+result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int element, int low, int high)
+    {
+        Arrays.sort(arr);
+        while(low <= high)
+        {
+            int middle = low + (high-low)/2;
+
+            if(arr[middle] == element)
+            {
+                return middle;
+            }
+            else if(arr[middle] > element)
+            {
+                return binarySearch(arr,element,low,middle-1);
+            }
+            else {
+                return binarySearch(arr,element,middle-1,high);
+            }
+        }
+        return -1;
+    }
+}
---
+++ Algorithms/src/July01/sorting/OptimizedBubbleSort.java
@@ -0,0 +1,9 @@
+package July01.sorting;
+
+public class OptimizedBubbleSort {
+
+    public static void main(String args[])
+    {
+
+    }
+}
