e009b21e8ef60de3b7bd9b1ac414e06818a3a5af
parasharparth <parthparashar6@gmail.com>
2022-07-04 01:34:10 -0700

    July-03-sorting

--- Algorithms/src/july03/Sorting/MergeSort.java
+++ Algorithms/src/july03/Sorting/MergeSort.java
@@ -44,6 +44,49 @@ public class MergeSort {
         int length2 = high-low;
 
         //Create the two arrays
-        //
+        int[] arr1 = new int[length1];
+        int[] arr2 = new int[length2];
+
+        //Populating the new arrays
+        for(int i=0; i<length1; i++)
+        {
+            arr1[i] = arr[i+low];
+        }
+        for(int j=0; j<length2; j++)
+        {
+            arr2[j] = arr[middle + j + 1];
+        }
+
+        //The next step is to check the elements and populate the new array
+        int i = 0;
+        int j= 0;
+        int k = low;
+        while(i<length1 && j<length2)
+        {
+            if(arr[i] < arr[j])
+            {
+                arr[k] = arr1[i];
+                i++;
+            }
+            else {
+                arr[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<length1)
+        {
+            arr[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        while(j<length2)
+        {
+            arr[k] = arr2[j];
+            j++;
+            k++;
+        }
     }
 }
