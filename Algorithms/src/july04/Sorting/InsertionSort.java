797ccd4c37ec1865a0bb971f94b934ba8d8352bf
parasharparth <parthparashar6@gmail.com>
2022-07-05 13:45:02 -0700

    July-05-sorting

---
+++ Algorithms/src/july04/Sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+package july04.Sorting;
+
+import javax.sound.midi.SysexMessage;
+
+public class InsertionSort {
+
+    public static void main(String[] args)
+    {
+        //The first step is to get the input array
+        int[] arr = {0,9,8,7,6,5,4,3,2,1};
+
+        //The second step is to print the unsorted array;
+        System.out.println("The unsorted array is: ");
+        for(int i=0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+",");
+        }
+        System.out.println();
+
+        //The third step is to call the insertion sort function
+
+    }
+}
