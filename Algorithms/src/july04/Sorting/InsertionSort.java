6608f99378ed5b1a91feda442410b84270c38317
parasharparth <parthparashar6@gmail.com>
2022-07-05 14:38:39 -0700

    July-05-sorting

--- Algorithms/src/july04/Sorting/InsertionSort.java
+++ Algorithms/src/july04/Sorting/InsertionSort.java
@@ -18,6 +18,28 @@ public class InsertionSort {
         System.out.println();
 
         //The third step is to call the insertion sort function
+        insertionSort(arr);
 
+        //The fourth step is to print the sorted array
+        System.out.println("The sorted array is: ");
+        for(int i=0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+",");
+        }
+    }
+
+    public static void insertionSort(int[] arr)
+    {
+        for(int i=1; i<arr.length; i++)
+        {
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];
+                --j;
+            }
+            arr[j+1] = key;
+        }
     }
 }
---
+++ Algorithms/src/july04/Sorting/MergeSort.java
@@ -0,0 +1,89 @@
+package july04.Sorting;
+
+public class MergeSort {
+
+    public static void main(String[] args)
+    {
+        //The first step is to get the input array
+        int[] arr = {0,9,8,7,6,5,4,3,2,1};
+
+        //The second step is to print the sorted array
+        System.out.println("The unsorted array is: ");
+        for(int i=0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+",");
+        }
+        System.out.println();
+
+        //The third step is to call merge sort
+        mergeSort(arr,0,arr.length-1);
+
+        //The fourth step is to print the sorted array
+        System.out.println("The sorted array is: ");
+        for(int i=0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+",");
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high)
+    {
+        if(low<high)
+        {
+            int middle = low + (high-low)/2;
+            mergeSort(arr,0,middle);
+            mergeSort(arr,middle+1,high);
+            merge(arr,low,middle,high);
+        }
+    }
+
+    public static void merge(int[] arr, int low, int middle, int high)
+    {
+        //The first step is to declare the new arrays
+        int length1 = middle - low +1;
+        int length2 = high-middle;
+
+        int[] arr1 = new int[length1];
+        int[] arr2 = new int[length2];
+
+        for(int i=0; i<length1; i++)
+        {
+            arr1[i] = arr[low+i];
+        }
+        for(int j=0; j<length2;j++)
+        {
+            arr2[j] = arr[middle+j+1];
+        }
+
+        int i=0;
+        int j=0;
+        int k = low;
+        while(i<length1 && j<length2)
+        {
+            if(arr1[i] <arr2[j])
+            {
+                arr[k] = arr1[i];
+                i++;
+            }
+            else {
+                arr[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<length1)
+        {
+            arr[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        while(j<length2)
+        {
+            arr[k] = arr2[j];
+            j++;
+            k++;
+        }
+    }
+}
