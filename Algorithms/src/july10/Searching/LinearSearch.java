aa3c7d1b9ed6c1f7b1a2c9c0eaf6bdaf971db687
parasharparth <parthparashar6@gmail.com>
2022-07-10 23:30:45 -0700

    july-10-searching

---
+++ Algorithms/src/july10/Searching/LinearSearch.java
@@ -0,0 +1,18 @@
+package july10.Searching;
+
+import july10.utils.Utils;
+
+public class LinearSearch {
+
+    public static void main(String[] args)
+    {
+        //The first step is to get the input array
+        int[] arr = Utils.getArray();
+        Utils.printArray(arr,1);
+
+        //The second step is to call the linear search function
+        //linearSearch(arr,5);
+
+        //The third step is to
+    }
+}
