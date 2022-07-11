6a354845c105682c0fc785ffeca61b54900c9840
parasharparth <parthparashar6@gmail.com>
2022-07-11 14:55:21 -0700

    july-10-sorting

---
+++ Algorithms/src/july10/Sorting/InsertionSort.java
@@ -0,0 +1,29 @@
+package july10.Sorting;
+
+import july10.utils.Utils;
+
+public class InsertionSort {
+
+    public static void main(String[] args)
+    {
+        int[] arr = Utils.getArray();
+        Utils.printArray(arr,2);
+        insertionSort(arr);
+        Utils.printArray(arr,3);
+    }
+
+    public static void insertionSort(int[] arr)
+    {
+        for(int i=1;i<arr.length;i++)
+        {
+            int key = arr[i];
+            int j=i-1;
+            while(j>=0 && key < arr[j])
+            {
+                arr[j+1] = arr[j];
+                --j;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
---
+++ Algorithms/src/july10/Sorting/QuickSort.java
@@ -0,0 +1,43 @@
+package july10.Sorting;
+
+import july07.utils.Utils;
+
+public class QuickSort {
+    public static void main(String[] args)
+    {
+        int[] arr = Utils.getArray();
+        Utils.printArray(arr,2);
+        quickSort(arr,0,arr.length-1);
+        Utils.printArray(arr,3);
+    }
+
+    public static void quickSort(int[] arr, int low, int high)
+    {
+        if(low<high)
+        {
+            int pivot = partition(arr,low,high);
+            quickSort(arr,low,pivot-1);
+            quickSort(arr,pivot+1,high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low-1;
+        for(int j=low; j<high ; j++)
+        {
+            if(arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[high];
+        arr[high] = arr[i+1];
+        arr[i+1] = temp;
+        return i+1;
+    }
+}
