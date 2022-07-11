9ced83902a554d1df82f3981bb124d5830013d74
parasharparth <parthparashar6@gmail.com>
2022-07-11 15:10:49 -0700

    july-11-searching

---
+++ Algorithms/src/july11/searching/LinearSearch.java
@@ -0,0 +1,25 @@
+package july11.searching;
+
+import july11.utils.Utils;
+
+public class LinearSearch {
+    public static void main(String[] args)
+    {
+        int[] arr = Utils.getArray();
+        Utils.printArray(arr,1);
+        int result = linearSearch(arr,5);
+        Utils.searchResult(result);
+    }
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length;i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Algorithms/src/july11/searching/RecursiveBinarySearch.java
@@ -0,0 +1,34 @@
+package july11.searching;
+
+import july11.utils.Utils;
+import java.util.Arrays;
+public class RecursiveBinarySearch {
+    public static void main(String[] args)
+    {
+        int[] arr = Utils.getArray();
+        Utils.printArray(arr,1);
+        int result = binarySearch(arr,5,0,arr.length-1);
+        Utils.searchResult(result);
+    }
+
+    public static int binarySearch(int[] arr, int element, int low, int high)
+    {
+        Arrays.sort(arr);
+        if(low<=high)
+        {
+            int middle = low + (high-low)/2;
+            if(arr[middle] == element)
+            {
+                return middle;
+            }
+            else if(arr[middle] > element)
+            {
+                return binarySearch(arr,element,low,middle-1);
+            }
+            else {
+                return binarySearch(arr,element,middle+1,high);
+            }
+        }
+        return -1;
+    }
+}
