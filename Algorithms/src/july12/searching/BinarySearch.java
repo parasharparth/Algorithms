6275e3dc3305e2bcda8d3fd60eb4fe97b9e11852
parasharparth <parthparashar6@gmail.com>
2022-07-12 01:28:32 -0700

    july-12-searching

---
+++ Algorithms/src/july12/searching/BinarySearch.java
@@ -0,0 +1,39 @@
+package july12.searching;
+
+import july12.utils.Utils;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static void main(String[] args)
+    {
+        int[] arr = Utils.getArray();
+        Utils.printArray(arr,1);
+        int result = binarySearch(arr,5,0,arr.length-1);
+        Utils.searchResult(result);
+    }
+
+    public static int binarySearch(int[] arr, int element, int low, int high)
+    {
+        Arrays.sort(arr);
+        while(low <= high)
+        {
+            int middle = low + (high - low) / 2;
+            if (arr[middle] == element)
+            {
+                return middle;
+            }
+            else if (arr[middle] > element)
+            {
+                high = middle - 1;
+            }
+            else
+            {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
