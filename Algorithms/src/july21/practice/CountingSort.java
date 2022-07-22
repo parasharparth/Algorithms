81f5266bf19e1b6c7b146b29c74b54045395ea90
parasharparth <parthparashar6@gmail.com>
2022-07-22 00:47:51 -0700

    july-21-counting-sort-practice

---
+++ Algorithms/src/july21/practice/CountingSort.java
@@ -0,0 +1,55 @@
+package july21.practice;
+
+import july21.utils.Utils;
+
+public class CountingSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(countingSort(Utils.getArray()),3);
+    }
+
+    public static int[] countingSort(int[] arr)
+    {
+        //The first step is to find out the maximum element of the array
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+
+        //The second step is to define and initialize the count array
+        int[] count = new int[max+1];
+        for(int i=0; i<arr.length;i++)
+        {
+            count[i] = 0;
+        }
+
+        //The third step is to populate the count array
+        for(int i=0; i<arr.length;i++)
+        {
+            count[arr[i]]++;
+        }
+
+        //The fourth step is to find the cumulative sum for the count array
+        for(int i=1; i<arr.length; i++)
+        {
+            count[i] += count[i-1];
+        }
+
+        //The fifth step is to calculate the index
+        int[] output = new int[arr.length];
+        for(int i=0; i<arr.length;i++)
+        {
+            output[count[arr[i]]-1] = arr[i];
+            count[arr[i]]--;
+        }
+
+        return output;
+    }
+}
