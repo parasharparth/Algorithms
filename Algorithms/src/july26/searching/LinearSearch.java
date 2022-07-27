f60092ed36e34d214656624ee6d60f88291db277
parasharparth <parthparashar6@gmail.com>
2022-07-27 16:42:06 -0700

    july-26-linear-search

---
+++ Algorithms/src/july26/searching/LinearSearch.java
@@ -0,0 +1,27 @@
+package july26.searching;
+
+import july26.utils.Utils;
+import java.util.ArrayList;
+
+public class LinearSearch {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.searchResult(linearSearch(Utils.getArray(),5));
+    }
+
+    public static int linearSearch(int[] arr,int element)
+    {
+        //The first step is to convert the given array into arrayList
+        ArrayList<Integer> list = Utils.convert(arr);
+        for(int i=0; i<list.size(); i++)
+        {
+            if(list.get(i) == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
