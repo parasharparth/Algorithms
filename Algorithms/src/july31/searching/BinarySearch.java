4e28625a43b6adf6db204433fc85cd8e96eb6b16
parasharparth <parthparashar6@gmail.com>
2022-07-31 23:47:46 -0700

    july-31-binary-search

---
+++ Algorithms/src/july31/searching/BinarySearch.java
@@ -0,0 +1,34 @@
+package july31.searching;
+
+import july31.utils.Utils;
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),1);
+        Utils.searchResult(binarySearch(Utils.getArray(),5,0,Utils.getArray().length-1));
+    }
+
+    public static int binarySearch(int[] arr, int element, int low, int high)
+    {
+        Arrays.sort(arr);
+        while(low<=high)
+        {
+            int middle = low + (high-low)/2;
+            if(arr[middle] == element)
+            {
+                return middle;
+            }
+            else if(arr[middle] > element)
+            {
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
