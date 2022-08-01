94612f9e2939e160cda6eea0762b2a8b3b8315e6
parasharparth <parthparashar6@gmail.com>
2022-07-31 23:49:29 -0700

    july-31-counting-sort

---
+++ Algorithms/src/july31/sorting/CountingSort.java
@@ -0,0 +1,12 @@
+package july31.sorting;
+
+import july31.utils.Utils;
+
+public class CountingSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+    }
+
+}
