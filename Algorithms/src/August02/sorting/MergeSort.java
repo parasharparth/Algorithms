800e65b76a110a1eff82e69e8828d1dd7840da17
parasharparth <parthparashar6@gmail.com>
2022-08-08 19:09:14 -0700

    August-02-merge-sort

---
+++ Algorithms/src/August02/sorting/MergeSort.java
@@ -0,0 +1,14 @@
+package August02.sorting;
+
+import August02.utils.Utils;
+
+public class MergeSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        //Utils.printArray(mergeSort(Utils.getArray(),0,Utils.getArray().length-1),3);
+    }
+
+//    public static
+}
