d1944e0c676a9baa9029624eaf799d4b598ca4ba
parasharparth <parthparashar6@gmail.com>
2022-08-17 16:23:26 -0700

    August-17-binary-Search

---
+++ Algorithms/src/august17/searching/BinarySearch.java
@@ -0,0 +1,37 @@
+package august17.searching;
+
+import august17.utils.Utils;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),1);
+        System.out.println("Enter the number you want to search");
+        Utils.searchResult(binarySearch(Utils.getArray(),new Scanner(System.in).nextInt(),0,Utils.getArray().length-1));
+    }
+
+    public static int binarySearch(int[] arr, int element, int low, int high)
+    {
+        Arrays.sort(arr);
+        while(low<=high)
+        {
+            int middle = low + (high - low)/2;
+            if(arr[middle] == element)
+            {
+                return middle;
+            }
+            else if(arr[middle] > element)
+            {
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
