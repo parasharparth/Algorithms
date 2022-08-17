0501c12c90c897cbb80ae7719177cddea8e6f614
parasharparth <parthparashar6@gmail.com>
2022-08-17 16:24:15 -0700

    August-17-counting-sort

---
+++ Algorithms/src/august17/sorting/CountingSort.java
@@ -0,0 +1,44 @@
+package august17.sorting;
+
+import august17.utils.Utils;
+
+public class CountingSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(countingSort(Utils.getArray()),3);
+    }
+
+    public static int[] countingSort(int[] arr)
+    {
+        int max = arr[0];
+        for(int i=1; i<arr.length ; i++)
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        int[] count = new int[max+1];
+        for(int j=0; j<count.length; j++)
+        {
+            count[j] = 0;
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+            count[arr[i]]++;
+        }
+        for(int i=1; i<arr.length; i++)
+        {
+            count[i] += count[i-1];
+        }
+        int[] output = new int[arr.length];
+        for(int i=0; i<arr.length; i++)
+        {
+            output[count[arr[i]]-1] = arr[i];
+            count[arr[i]]--;
+        }
+        return output;
+    }
+}
