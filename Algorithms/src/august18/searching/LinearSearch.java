ce059cd3a5f426e5b7e20bbfed7134c0b4cf1678
parasharparth <parthparashar6@gmail.com>
2022-08-18 05:07:10 -0700

    August-18-Linear-Search

---
+++ Algorithms/src/august18/searching/LinearSearch.java
@@ -0,0 +1,24 @@
+package august18.searching;
+
+import august18.utils.Utils;
+
+public class LinearSearch {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),1);
+        Utils.searchResult(linearSearch(Utils.getArray(),5));
+    }
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length-1; i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
