f5df55eec22185c7cb1644ad8c7b9bcad3c81235
parasharparth <parthparashar6@gmail.com>
2022-08-30 03:59:51 -0700

    August-30-Insertion-Sort

---
+++ Algorithms/src/august30/sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+package august30.sorting;
+
+import august30.utils.Utils;
+
+public class InsertionSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(insertionSort(Utils.getArray()),3);
+    }
+
+    public static int[] insertionSort(int[] arr)
+    {
+        for(int i=1; i<arr.length; i++)
+        {
+            int j=i-1;
+            int key = arr[i];
+            while(j >= 0 && key < arr[j])
+            {
+                arr[j+1] = arr[j];
+                --j;
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+}
