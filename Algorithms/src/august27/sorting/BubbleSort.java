f879498c948d6b5e6e8b6da50f7e12b9f7e68c2a
parasharparth <parthparashar6@gmail.com>
2022-08-30 02:42:35 -0700

    August-27-Bubble-Sort

---
+++ Algorithms/src/august27/sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package august27.sorting;
+
+import august27.utils.Utils;
+
+public class BubbleSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(bubbleSort(Utils.getArray()),3);
+    }
+
+    public static int[] bubbleSort(int[] arr)
+    {
+        for(int i=0; i<arr.length-1; i++)
+        {
+            for(int j=0; j<arr.length-i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
