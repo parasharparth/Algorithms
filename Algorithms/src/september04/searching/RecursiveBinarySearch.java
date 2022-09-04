553672fbd530f116dc7f920e5a3db0d9caa67374
parasharparth <parthparashar6@gmail.com>
2022-09-04 15:21:22 -0700

    september-04-recursive-binary-Search

---
+++ Algorithms/src/september04/searching/RecursiveBinarySearch.java
@@ -0,0 +1,36 @@
+package september04.searching;
+
+import september03.utils.Utils;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RecursiveBinarySearch {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),1);
+        Utils.searchResult(binarySearch(Utils.getArray(),new Scanner(System.in).nextInt(),0,Utils.getArray().length-1));
+    }
+
+    public static int binarySearch(int[] arr, int element, int low, int high)
+    {
+        Arrays.sort(arr);
+        if(low<=high)
+        {
+            int middle = low + (high - low)/2;
+            if(arr[middle] == element)
+            {
+                return middle;
+            }
+            else if(arr[middle] > element)
+            {
+                return binarySearch(arr,element,low,middle-1);
+            }
+            else {
+                return binarySearch(arr,element,middle+1,high);
+            }
+        }
+        return -1;
+    }
+}
