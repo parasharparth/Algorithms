748b78c02fededdf5dd7f9d6aa7cab5e9c7def87
parasharparth <parthparashar6@gmail.com>
2022-09-20 11:47:08 -0700

    september19-Linear-search

---
+++ Algorithms/src/september19/searching/LinearSearch.java
@@ -0,0 +1,26 @@
+package september19.searching;
+
+import september19.utils.Utils;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),1);
+        Utils.searchResult(linearSearch(Utils.getArray(), new Scanner(System.in).nextInt()));
+    }
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
