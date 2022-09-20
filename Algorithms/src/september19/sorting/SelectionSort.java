69b643c2a64781b75aacb674bc1d81e614f526a9
parasharparth <parthparashar6@gmail.com>
2022-09-20 11:47:49 -0700

    september19-selection-sort

---
+++ Algorithms/src/september19/sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package september19.sorting;
+
+import september19.utils.Utils;
+
+public class SelectionSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(selectionSort(Utils.getArray()),3);
+    }
+
+    public static int[] selectionSort(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            int minimum_index = i;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[j] < arr[minimum_index])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[minimum_index];
+                    arr[minimum_index] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
