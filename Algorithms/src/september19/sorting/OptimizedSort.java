3029cb67f9cb6dd1c7b130047963124ed2cb5a69
parasharparth <parthparashar6@gmail.com>
2022-09-20 11:48:39 -0700

    september19-optimized-sort

---
+++ Algorithms/src/september19/sorting/OptimizedSort.java
@@ -0,0 +1,35 @@
+package september19.sorting;
+
+import september19.utils.Utils;
+
+public class OptimizedSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(bubbleSort(Utils.getArray()),3);
+    }
+
+    public static int[] bubbleSort(int[] arr)
+    {
+        for(int i=0; i<arr.length-1; i++)
+        {
+            boolean swapped = false;
+            for(int j=0; j<arr.length-i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+            {
+                break;
+            }
+        }
+        return arr;
+    }
+}
