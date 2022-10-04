470d029d9b39667a2d3ab2144d922990b7b30b5c
parasharparth <parthparashar6@gmail.com>
2022-10-04 15:07:20 -0700

    october-04-selection-sort

---
+++ Algorithms/src/october04/sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package october04.sorting;
+
+import october04.utils.Utils;
+
+public class SelectionSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(selectionSort(Utils.getArray()),3);
+    }
+
+    public static int[] selectionSort(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            int minimum_index = i;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[minimum_index] > arr[j])
+                {
+                    minimum_index = j;
+                }
+            }
+            int temp = arr[minimum_index];
+            arr[minimum_index] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+}
