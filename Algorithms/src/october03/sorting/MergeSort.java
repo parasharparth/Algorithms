e17d1b4f4d0cd90dff24b7706166347096ac279b
parasharparth <parthparashar6@gmail.com>
2022-10-03 23:49:21 -0700

    october-03-merge-sort

---
+++ Algorithms/src/october03/sorting/MergeSort.java
@@ -0,0 +1,17 @@
+package october03.sorting;
+
+import october03.utils.Utils;
+
+public class MergeSort {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),2);
+        Utils.printArray(mergeSort(Utils.getArray(), 0, Utils.getArray().length-1),3);
+    }
+
+    public static int[] mergeSort(int[] arr, int low, int high)
+    {
+       return null;
+    }
+}
