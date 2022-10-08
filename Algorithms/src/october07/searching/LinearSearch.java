264c95e34232dd61cca0171f57112c0fe3523141
parasharparth <parthparashar6@gmail.com>
2022-10-07 23:37:21 -0700

    october-07

---
+++ Algorithms/src/october07/searching/LinearSearch.java
@@ -0,0 +1,26 @@
+package october07.searching;
+
+import october07.utils.Utils;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args)
+    {
+        Utils.printArray(Utils.getArray(),1);
+        Utils.searchResult(linearSearch(Utils.getArray(), new Scanner(System.in).nextInt()));
+    }
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
