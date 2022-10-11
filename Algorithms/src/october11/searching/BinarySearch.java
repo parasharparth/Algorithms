ab67709877a570f78e4d054ff9196f7c93e8a088
parasharparth <parthparashar6@gmail.com>
2022-10-11 08:38:27 -0700

    october-11-Static-Binary-Fixed

--- Algorithms/src/october11/searching/BinarySearch.java
+++ Algorithms/src/october11/searching/BinarySearch.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BinarySearch {
 
-    public static int binarySearch(int[] arr, int element, int low, int high)
+    public int binarySearch(int[] arr, int element, int low, int high)
     {
         Arrays.sort(arr);
         while(low<=high)
