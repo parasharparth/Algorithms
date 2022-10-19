5495de1240a55e4140ad16503ba6905557971a34
parasharparth <parthparashar6@gmail.com>
2022-10-19 03:15:29 -0700

    october-19-linear-search

---
+++ Algorithms/src/october19/searching/LinearSearch.java
@@ -0,0 +1,16 @@
+package october19.searching;
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
