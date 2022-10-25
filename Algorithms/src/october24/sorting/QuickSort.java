e1b40e5cc75fcdeb4d35898592f72521b20fdec9
parasharparth <parthparashar6@gmail.com>
2022-10-24 23:58:57 -0700

    october-24-all

---
+++ Algorithms/src/october24/sorting/QuickSort.java
@@ -0,0 +1,20 @@
+package october24.sorting;
+
+public class QuickSort {
+
+    public static int[] quickSort(int[] arr, int low, int high)
+    {
+        if(low<high)
+        {
+            int pivot = partition(arr, low, high);
+            quickSort(arr,low,pivot-1);
+            quickSort(arr,pivot+1, high);
+        }
+        return arr;
+    }
+
+    public static int partition(int[] arr, int low, int high)
+    {
+        return 1;
+    }
+}
---
+++ Algorithms/src/october24/sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+package october24.sorting;
+
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            int minimum_index = i;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[minimum_index] > arr[j])
+                {
+                    minimum_index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minimum_index];
+            arr[minimum_index] = temp;
+        }
+        return arr;
+    }
+}
