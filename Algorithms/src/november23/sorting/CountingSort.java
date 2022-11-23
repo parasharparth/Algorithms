bf924319259899c03316dedf0d869a56fc1405b3
parasharparth <parthparashar6@gmail.com>
2022-11-23 03:50:27 -0800

    november-23-Counting-Sort

---
+++ Algorithms/src/november23/sorting/CountingSort.java
@@ -0,0 +1,26 @@
+package november23.sorting;
+
+import november23.utils.Utils;
+
+public class CountingSort {
+
+    public static int[] countingSort(int[] arr)
+    {
+        int[] count = new int[Utils.findMax(arr)+1];
+        for(int i:arr)
+        {
+            count[arr[i]]++;
+        }
+        for(int i=1; i<arr.length; i++)
+        {
+            count[i] += count[i-1];
+        }
+        int[] output = new int[arr.length];
+        for(int i:arr)
+        {
+            output[count[arr[i]]-1] = arr[i];
+            count[arr[i]]--;
+        }
+        return arr;
+    }
+}
