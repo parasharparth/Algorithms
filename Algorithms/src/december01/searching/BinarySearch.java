e4a5a53fcc4489abe9b69347846e73a4bbbe25d5
parasharparth <parthparashar6@gmail.com>
2022-12-03 00:01:47 -0800

    december-2

---
+++ Algorithms/src/december01/searching/BinarySearch.java
@@ -0,0 +1,17 @@
+package december01.searching;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int element, int low, int high)
+    {
+        Arrays.sort(arr);
+        while (low <= high)
+        {
+            int middle = low + (high - low)/2;
+
+        }
+        return 1;
+    }
+}
---
+++ Algorithms/src/december01/searching/LinearSearch.java
@@ -0,0 +1,16 @@
+package december01.searching;
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
