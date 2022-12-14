9603cb16186d125a6ee3f5a1bf310fc4a4cd7ae6
parasharparth <parthparashar6@gmail.com>
2022-12-14 13:09:14 -0800

    Linear-search

---
+++ Algorithms/src/december13/searching/LinearSearch.java
@@ -0,0 +1,18 @@
+package december13.searching;
+
+import java.util.Arrays;
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
