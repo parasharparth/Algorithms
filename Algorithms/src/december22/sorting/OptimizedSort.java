0a2f462cd3c0e498fc95df79a36d24df49c152e9
parasharparth <parthparashar6@gmail.com>
2022-12-22 02:08:20 -0800

    December-22-optimized-sort

---
+++ Algorithms/src/december22/sorting/OptimizedSort.java
@@ -0,0 +1,27 @@
+package december22.sorting;
+
+public class OptimizedSort {
+
+    public static int[] optimizedSort(int[] arr)
+    {
+        for(int i=0; i<arr.length-1; i++)
+        {
+            boolean swapped = false;
+            for(int j=0; j<arr.length-i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+            {
+                break;
+            }
+        }
+        return arr;
+    }
+}
