5da185e67d34a7dc83c4283f6ed2d8847dca9ecf
parasharparth <parthparashar6@gmail.com>
2023-01-04 02:33:41 -0800

    January-04-Selection-Sort

---
+++ Algorithms/src/CY2023/January/jan04/sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+package CY2023.January.jan04.sorting;
+
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            int minimum_index = i;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[j] < arr[minimum_index])
+                {
+                    minimum_index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minimum_index];
+            arr[minimum_index] = temp;
+        }
+        return arr;
+    }
+}
