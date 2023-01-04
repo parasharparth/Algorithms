f41328582f3faf3f44b79503906edc26297f729c
parasharparth <parthparashar6@gmail.com>
2023-01-04 01:38:21 -0800

    January-03-Bubble-sort

---
+++ Algorithms/src/CY2023/January/Jan03/sorting/BubbleSort.java
@@ -0,0 +1,19 @@
+package CY2023.January.Jan03.sorting;
+
+public class BubbleSort {
+
+    public static int[] bubbleSort(int[] arr)
+    {
+        for(int i=0; i<arr.length-1; i++)
+        {
+            for(int j=0; j<arr.length-i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+
+                }
+            }
+        }
+        return arr;
+    }
+}
