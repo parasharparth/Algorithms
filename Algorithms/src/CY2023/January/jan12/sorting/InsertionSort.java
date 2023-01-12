36adebc6e6e5c57a7917c3fc230b05c3ecf1a085
parasharparth <parthparashar6@gmail.com>
2023-01-12 15:23:17 -0800

    January-12-InsertionSort

---
+++ Algorithms/src/CY2023/January/jan12/sorting/InsertionSort.java
@@ -0,0 +1,20 @@
+package CY2023.January.jan12.sorting;
+
+public class InsertionSort {
+
+    public static int[] insertionSort(int[] arr)
+    {
+        for(int i=1; i<arr.length; i++)
+        {
+            int key = arr[i];
+            int j=i-1;
+            while(j>=0 && key<arr[j])
+            {
+                arr[j+1] = arr[j];
+                --j;
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+}
