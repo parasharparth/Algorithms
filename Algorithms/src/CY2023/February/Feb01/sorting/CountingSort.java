3a21313b308b641a92c907e5ece5c86e15afb806
parasharparth <parthparashar6@gmail.com>
2023-02-01 03:08:01 -0800

    Feb-01-Counting-Sort

---
+++ Algorithms/src/CY2023/February/Feb01/sorting/CountingSort.java
@@ -0,0 +1,21 @@
+package CY2023.February.Feb01.sorting;
+
+import CY2023.utils.Utils;
+
+public class CountingSort {
+	public static int[] countingSort(int[] arr){
+		int[] count = new int[Utils.findMax(arr)+1];
+		for(int i:arr){
+			count[arr[i]]++;
+		}
+		for(int i=1; i<arr.length; i++){
+			count[i] += count[i-1];
+		}
+		int[] output = new int[arr.length];
+		for(int i:arr){
+			output[count[arr[i]]-1] = arr[i];
+			count[arr[i]]--;
+		}
+		return output;
+	}
+}
