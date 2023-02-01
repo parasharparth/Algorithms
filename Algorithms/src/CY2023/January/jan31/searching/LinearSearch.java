22f7a02b0e5e9d38e17ea83782c1f6f17ae895e2
parasharparth <parthparashar6@gmail.com>
2023-02-01 02:13:18 -0800

    Jan-31-Linear-Search

---
+++ Algorithms/src/CY2023/January/jan31/searching/LinearSearch.java
@@ -0,0 +1,16 @@
+package CY2023.January.jan31.searching;
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int element)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
