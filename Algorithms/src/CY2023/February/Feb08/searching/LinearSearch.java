e95bbf81110aa75dd5055e01bf4096be4e97e73a
parasharparth <parthparashar6@gmail.com>
2023-02-08 19:10:24 -0800

    Feb-08-Linear-Search

---
+++ Algorithms/src/CY2023/February/Feb08/searching/LinearSearch.java
@@ -0,0 +1,20 @@
+package CY2023.February.Feb08.searching;
+
+public class LinearSearch {
+
+	public static void main(String[] args){
+		int[] arr = new int[]{1,3,4,2,5,8,7,6};
+		int result = linearSearch(arr,4);
+		if(result == -1)
+			System.out.println("Not found");
+		else
+			System.out.println("Found at position: "+result+1);
+	}
+
+	public static int linearSearch(int[] arr, int element){
+		for(int i=0; i<arr.length; i++)
+			if(arr[i]==element)
+				return i;
+		return -1;
+	}
+}
