39933c58bff59a26158efdf666b2b5171cfc6376
parasharparth <parthparashar6@gmail.com>
2023-02-27 23:44:59 -0800

    Feb-27-Algorithms

---
+++ Algorithms/src/CY2023/February/Feb27/sorting/SelectionSort.java
@@ -0,0 +1,15 @@
+package CY2023.February.Feb27.sorting;
+
+public class SelectionSort {
+
+	public static void main(String[] args){
+		int[] arr = new int[]{1,3,4,2,5,8,7,6};
+		selectionSort(arr);
+		for (int j =0; j < arr.length; j++) System.out.print(arr[j] + " ");
+		System.out.println();
+	}
+
+	public static void selectionSort(int[] arr){
+
+	}
+}
