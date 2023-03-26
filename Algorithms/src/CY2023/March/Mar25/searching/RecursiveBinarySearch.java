a252f8d96b51289f4dfc16d3fb213fe8cb4a18a3
parasharparth <parthparashar6@gmail.com>
2023-03-25 17:42:02 -0700

    Mar25-Recursive-Binary-Search

---
+++ Algorithms/src/CY2023/March/Mar25/searching/RecursiveBinarySearch.java
@@ -0,0 +1,35 @@
+package CY2023.March.Mar25.searching;
+
+import java.util.Arrays;
+
+public class RecursiveBinarySearch {
+
+	public static void main(String[] args){
+		int[] arr = new int[]{1,5,4,3,6,8,2};
+		int result = binarySearch(arr,2,0,arr.length-1);
+		if(result!= -1){
+			System.out.println("Element found on position "+ result);
+		}
+		else
+		{
+			System.out.println("Element not found");
+		}
+	}
+
+	public static int binarySearch(int[] arr, int element, int low, int high){
+		Arrays.sort(arr);
+		if(low <= high){
+			int middle = low + (high - low)/2;
+			if(arr[middle] == element){
+				return middle;
+			}
+			else if(arr[middle] > element){
+				return binarySearch(arr, element, low, middle - 1);
+			}
+			else {
+				return binarySearch(arr, element, middle+1, high);
+			}
+		}
+		return -1;
+	}
+}
