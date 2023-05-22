eb72d4a04067187ea5eabecacb9c09bff30fd938
parasharparth <parthparashar6@gmail.com>
2023-05-22 14:32:50 -0700

    Linear Search

---
+++ Algorithms/src/CY2023/May/May11/LinearSearch.java
@@ -0,0 +1,13 @@
+package CY2023.May.May11;
+
+import java.util.ArrayList;
+
+public class LinearSearch {
+
+	public static void main(String[] args){
+		int[] arr = new int[]{0,9,8,7,6,5,4,3,2,1};
+		ArrayList al = new ArrayList();
+		al.add(3);
+		al.add("shweta");
+	}
+}
