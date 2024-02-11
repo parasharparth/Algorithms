70806a5ec0f3e0fa9473e9a71b1afc932e7566b6
parasharparth <parthparashar6@gmail.com>
2024-02-11 18:03:43 -0500

    trying to get back

---
+++ Algorithms/src/CY2024/January/January05/LinearSearch.java
@@ -0,0 +1,13 @@
+package CY2024.January.January05;
+
+import java.util.Arrays;
+
+public class LinearSearch {
+	public static void main(String[] args){
+		int[] arr = new int[]{0,9,8,7,6,5,4,3,2,1};
+		System.out.println("----------------------Linear Search----------------------");
+		System.out.println("The given array is: ");
+		Arrays.stream(arr).forEach(System.out::println);
+		 
+	}
+}
